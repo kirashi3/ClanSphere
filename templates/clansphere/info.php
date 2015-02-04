004b29694c23bbf007eb0cf62bcf239f419b646e
sgraewe <stefan.graewe@gmail.com>
2015-02-04 03:12:26 +0100

    Add new default template structure

--- templates/clansphere/info.php
+++ templates/clansphere/info.php
@@ -4,6 +4,6 @@ $mod_info['name']     = 'ClanSphere 2015';
 $mod_info['version']  = $cs_main['version_name'];
 $mod_info['released'] = $cs_main['version_date'];
 $mod_info['creator']  = 'sgraewe';
-$mod_info['team']     = 'csphere.eu';
+$mod_info['team']     = 'csphere';
 $mod_info['url']      = 'http://www.csphere.eu';
 $mod_info['text']     = 'Default template';
