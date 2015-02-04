0a0985d6eff05ef466ecef0917ea672c12381e41
sgraewe <stefan.graewe@gmail.com>
2015-02-04 01:52:18 +0100

    Add bootstrap v3.3.2

---
+++ mods/bootstrap/info.php
@@ -0,0 +1,18 @@
+<?php
+
+$cs_lang = cs_translate('bootstrap');
+
+$mod_info['name']       = $cs_lang['mod_name'];
+$mod_info['version']    = 'v3.3.2';
+$mod_info['released']   = $cs_main['version_date'];
+$mod_info['creator']    = 'Bootstrap';
+$mod_info['team']       = 'Bootstrap';
+$mod_info['url']        = 'http://getbootstrap.com/';
+$mod_info['text']       = $cs_lang['mod_text'];
+$mod_info['icon']       = 'bootstrap';
+$mod_info['show']       = array();
+$mod_info['categories'] = FALSE;
+$mod_info['comments']   = FALSE;
+$mod_info['protected']  = TRUE;
+$mod_info['tables']     = array('');
+$mod_info['startup']    = TRUE;
---
+++ mods/bootstrap/startup.php
@@ -0,0 +1,4 @@
+<?php
+
+cs_scriptload('bootstrap', 'stylesheet', 'css/bootstrap.min.css');
+cs_scriptload('bootstrap', 'javascript', 'js/bootstrap.min.js');
