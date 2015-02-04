5aeaea754774a81f1b092e540250b9f5cf5dc932
sgraewe <stefan.graewe@gmail.com>
2015-02-04 01:10:59 +0100

    Change clansphere template info

--- templates/clansphere/info.php
+++ templates/clansphere/info.php
@@ -3,7 +3,7 @@
 $mod_info['name']     = 'ClanSphere';
 $mod_info['version']  = $cs_main['version_name'];
 $mod_info['released'] = $cs_main['version_date'];
-$mod_info['creator']  = 'Seron';
-$mod_info['team']     = 'Seron';
-$mod_info['url']      = 'http://www.fx-interfaces.com';
-$mod_info['text']     = 'Default Template';
+$mod_info['creator']  = 'sgraewe';
+$mod_info['team']     = 'csphere.eu';
+$mod_info['url']      = 'http://www.csphere.eu';
+$mod_info['text']     = 'Default template';
